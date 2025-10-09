8a9bebe96a06830a950026d78aa16ce5fc2d1568
Andrew Hall <andrew.hall@grafana.com>
2025-10-09 11:23:41 +0800

    Tweak comment
    Signed-off-by: Andrew Hall <andrew.hall@grafana.com>

--- promql/engine.go
+++ promql/engine.go
@@ -3944,8 +3944,8 @@ func preprocessExprHelper(expr parser.Expr, start, end time.Time) (isStepInvaria
 	case *parser.Call:
 		_, ok := AtModifierUnsafeFunctions[n.Func.Name]
 		isStepInvariant := !ok
-		// A special case to allow timestamp() to be wrapped in a step invariant based off it's argument.
-		// timestamp() is considered AtModifierUnsafe, but it can be safe depending on it's arguments.
+		// A special case to allow timestamp() to be wrapped in a step invariant.
+		// timestamp() is considered AtModifierUnsafe, but it can be safe depending on its arguments.
 		// ie timestamp(metric @ 1) is step invariant, but timestamp(abs(metric @ 1)) is not.
 		isTimestampWithAllArgsStepInvariantSafe := n.Func.Name == "timestamp"
 		shouldWrap := make([]bool, len(n.Args))
